eb557613abbb744bfbd56a86a589f8733cd59e05
Xifre Font <xifre.font.97@gmail.com>
2022-07-12 20:28:47 +0200

    Mega crazy solution with infinity

--- public/src/index.js
+++ public/src/index.js
@@ -76,4 +76,38 @@ const inputValue2 = function () {
     });
 };
 
-inputValue2();
+const inputValue3 = function () {
+    const testsToArray = Object.values(tests);
+    let iterations = 0;
+    let result;
+    let a;
+    let b;
+
+    testsToArray.forEach((test) => {
+        console.log("------------");
+        a = test[0];
+        b = test[1];
+
+        if (isNaN(a)) {
+            pullResult(!strictEquals(a, b), a, b, iterations);
+        } else if (
+            (1 / a).toString().includes("-") ||
+            (1 / b).toString().includes("-")
+        ) {
+            pullResult(!strictEquals(a, b), a, b, iterations);
+        } else {
+            pullResult(strictEquals(a, b), a, b, iterations);
+        }
+    });
+};
+
+inputValue3();
+
+// let getMinusA = 1 / a;
+// let getMinusB = 1 / b;
+// let infinityToStringA = getMinusA.toString();
+// let infinityToStringB = getMinusB.toString();
+// if (
+//     infinityToStringA.includes("-") ||
+//     infinityToStringB.includes("-")
+// ) {
